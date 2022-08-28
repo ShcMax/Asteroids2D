4a3043a687cdd279fcc55cf51f31ba969ce9eea8
ShcMax <102209342+ShcMax@users.noreply.github.com>
2022-08-28 18:33:31 +0300

    Day_5.1

--- Assets/Homework/CreationalPatternsPart2/Assets/Prototype/Scripts/PrototypeExample.cs
+++ Assets/Homework/CreationalPatternsPart2/Assets/Prototype/Scripts/PrototypeExample.cs
@@ -7,32 +7,50 @@ namespace Prototype.Scripts
         [SerializeField]
         private MyCloneableObject _origin;
 
+        int count = 5;
+        float timer = 2;
+        float _timeEnd = 0;
 
-        [ContextMenu("Test DotNet Cloneable")]
-        private void TestDotNetCloneable()
+        private void Update()
         {
-            var origin = PlayerDataFactory.CreateCloneablePlayerData();
-            var clone = (CloneablePlayerData)origin.Clone();
-
-            origin.Speed = 666;
-            origin.HealthData.CurrentHealth = 37;
-
-            Debug.Log($"PrototypeExample.TestDotNetCloneable: origin.Speed = {origin.Speed}; clone.Speed = {clone.Speed}");
-            Debug.Log($"PrototypeExample.TestDotNetCloneable: origin.CurrentHealth = {origin.HealthData.CurrentHealth}; clone.CurrentHealth = {clone.HealthData.CurrentHealth}");
+            timer = timer - Time.deltaTime;
+            for (int i = 0; i < count; i++)
+            {
+                if(timer <= _timeEnd)
+                {
+                    timer = 2;
+                    TestPrototype();
+                }
+            }  
         }
 
-        [ContextMenu("Test Deep Copy")]
-        private void TestDeepCopy()
-        {
-            var origin = PlayerDataFactory.CreatePlayerData();
-            var deepCopy = origin.DeepCopy();
+        //[ContextMenu("Test DotNet Cloneable")]
+        //private void TestDotNetCloneable()
+        //{
+        //    var origin = PlayerDataFactory.CreateCloneablePlayerData();
+        //    var clone = (CloneablePlayerData)origin.Clone();
 
-            origin.Speed = 42;
-            origin.HealthData.CurrentHealth = 43;
+        //    origin.Speed = 666;
+        //    origin.HealthData.CurrentHealth = 37;
 
-            Debug.Log($"PrototypeExample.TestDeepCopy: origin.Speed = {origin.Speed}; deepCopy.Speed = {deepCopy.Speed}");
-            Debug.Log($"PrototypeExample.TestDeepCopy: origin.CurrentHealth = {origin.HealthData.CurrentHealth}; deepCopy.CurrentHealth = {deepCopy.HealthData.CurrentHealth}");
-        }
+        //    Debug.Log($"PrototypeExample.TestDotNetCloneable: origin.Speed = {origin.Speed}; clone.Speed = {clone.Speed}");
+        //    Debug.Log($"PrototypeExample.TestDotNetCloneable: origin.CurrentHealth = {origin.HealthData.CurrentHealth}; clone.CurrentHealth = {clone.HealthData.CurrentHealth}");
+        //}
+
+        //[ContextMenu("Test Deep Copy")]
+        //private void TestDeepCopy()
+        //{
+        //    var origin = PlayerDataFactory.CreatePlayerData();
+        //    var deepCopy = origin.DeepCopy();
+
+        //    origin.Speed = 42;
+        //    origin.HealthData.CurrentHealth = 43;
+
+        //    Debug.Log($"PrototypeExample.TestDeepCopy: origin.Speed = {origin.Speed}; deepCopy.Speed = {deepCopy.Speed}");
+        //    Debug.Log($"PrototypeExample.TestDeepCopy: origin.CurrentHealth = {origin.HealthData.CurrentHealth}; deepCopy.CurrentHealth = {deepCopy.HealthData.CurrentHealth}");
+        //}
+
+
 
         [ContextMenu("Test Prototype")]
         private void TestPrototype()
@@ -42,11 +60,11 @@ namespace Prototype.Scripts
             Debug.Log("PrototypeExample.TestPrototype: newObjectCasted.name = " + newObjectCasted.name);
         }
         
-        [ContextMenu("Test Instantiate")]
-        private void TestInstantiate()
-        {
-            var newObject = Instantiate(_origin);
-            newObject.transform.position += Vector3.left;
-        }
+        //[ContextMenu("Test Instantiate")]
+        //private void TestInstantiate()
+        //{
+        //    var newObject = Instantiate(_origin);
+        //    newObject.transform.position += Vector3.left;
+        //}
     }
 }
